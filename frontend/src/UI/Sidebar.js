487a7eb01bc612811fc22eeef5f32ff334ba3dd8
trifine <trifine.laurensi08@gmail.com>
2023-05-23 23:14:43 +0700

    no message

--- frontend/src/UI/Sidebar.js
+++ frontend/src/UI/Sidebar.js
@@ -6,19 +6,12 @@ export const Sidebar = () => {
     <>
     <div className={classes.sidebar}>
         <nav className={classes["side-nav"]}>
-<<<<<<< HEAD
           <ul >
-=======
-          <div className={classes["sidetop"]}>
-          <ul>
->>>>>>> c8357c6bfeb1482e7f6dee081279983e24152188
             <Link to="/"><img src='../assets/home.svg'></img></Link>
             <Link to="/calender"><img src='../assets/icon1.svg'></img></Link>
             <Link to="/assignment"><img src='../assets/icon2.svg'></img></Link>
             <Link to="/history"><img src='../assets/icon3.svg'></img></Link>
           </ul>
-          </div>
-
             <div className={classes["sidedown"]}>
           <ul>
             <img src='../assets/image1.png'></img>
