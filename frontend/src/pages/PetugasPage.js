99ce5e7f5f60ab224759cd5cddd771cf810f94c7
JUAN\acer aspire 5 <erickuniv@gmail.com>
2023-05-26 22:13:48 +0700

    no message

--- frontend/src/pages/PetugasPage.js
+++ frontend/src/pages/PetugasPage.js
@@ -15,15 +15,23 @@ export const PetugasPage = () => {
 
             <p> <i class="fa fa-home" aria-hidden="true"></i> &nbsp; Home </p>
             <p> <i class="fa fa-users" aria-hidden="true"></i> &nbsp; Absensi </p>
-            <p> <i class="fa fa-qrcode" aria-hidden="true"></i> &nbsp; Scan Masuk </p>
-            <p> <i class="fa fa-qrcode" aria-hidden="true"></i> &nbsp; Scan Keluar </p>
             <p> <i class="fa fa-sign-out" aria-hidden="true"></i> &nbsp; Log out </p>
           </div>
 
           <div className={classes['mainup']}>  
             <div className={classes['upcont']}>
-            <h1>Dashboard Petugas</h1>
-            <p> Welcome back, Kenjod</p>
+            <h1>Dashboard </h1>
+            <p> Welcome back, Tipo</p>
+
+            <span>Website untuk petugas <br></br>Methodist Charles Wesley</span> 
+            <div className={classes['butt']}>Get Started</div>
+            <div className={classes['scanin']}>
+              <div>
+              <h1>Scan In</h1>
+              <p>Scan untuk masuk kedalam perpustakaan</p>
+              </div>
+            </div>
+
             </div>
 
           </div>
