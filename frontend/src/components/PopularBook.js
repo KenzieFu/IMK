a8e08e77e4f22674b2ab2f89abc2d0b370427d8f
LAPTOP-B0212VLT\User <kenziefubrianto@gmail.com>
2023-05-27 10:23:20 +0700

    no message

--- frontend/src/components/PopularBook.js
+++ frontend/src/components/PopularBook.js
@@ -2,18 +2,19 @@ import React from 'react'
 import classes from './PopularBook.module.css'
 import { Link } from 'react-router-dom';
 export const PopularBook = (props) => {
-  
-    const pop =props.books.filter((popular,index)=> (index%3 !==0 || index===0) );
-    const daily=pop.map((popular)=><Link to={`/library/${popular.id_buku}`}><li  id={popular.id}><img src={`http://localhost:8080${popular.gambar_buku}`} alt="" />
+    const pops=props.books.sort((a,b)=> b.jumlah_pinjam -a.jumlah_pinjam);
+    const pop =pops.filter((popular,index)=> (index%5 !==0 || index===0) );
+    const daily=pop.map((popular)=><Link to={`/library/${popular.id_buku}`}><li  id={popular.id}><img src={`http://localhost:8080${popular.buku.gambar_buku}`} alt="" />
     <div className={classes["book-detail"]}><h2>{popular.buku.judul_buku}</h2>
-    <span>{popular.buku.pengarang}</span></div>
+    <span>{popular.buku.pengarang}</span>
+    <span>Jumlah Dipinjam: {popular?.jumlah_pinjam}</span></div>
 </li></Link>)
 
   return (
     <>
     <div className={classes.popular}>
     <div className={classes.wibulu}> 
-      <h1>Pilihan Harian</h1>
+      <h1>Buku Terpopuler</h1>
       <img src="./assets/anime-jump.gif" alt="" />
       </div>
     <ul>
